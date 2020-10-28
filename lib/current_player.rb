3d758f21d3160d456e1c73b4079d84e4788b6dca
Sam Hall <mann.samantha11@gmail.com>
2020-10-28 19:06:31 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,8 +1,7 @@
 def turn_count(board)
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+# board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 counter = 0
 board.each do |turn|
+  if turn == "X" || "O"  
     counter += 1
-
-
 end
